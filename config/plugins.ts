e697e601038974506c497534ba9f6d98a00c8aa5
thstanton <thstanton@proton.me>
2024-04-05 15:50:19 +0100

    PSQL config

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,25 +3,27 @@ module.exports = ({ env }) => ({
     enabled: true,
     config: {
       acceptedNodeEnvs: ["development"],
-      commonInterfacesFolderName: 'schemas-to-ts',
+      commonInterfacesFolderName: "schemas-to-ts",
       alwaysAddEnumSuffix: false,
       alwaysAddComponentSuffix: false,
       usePrettierIfAvailable: true,
       logLevel: 2,
-      destinationFolder: 'src/common/schemas-to-ts',
-    }
+      destinationFolder: "src/common/schemas-to-ts",
+    },
   },
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          ACL: env('AWS_ACL', 'public-read'),
-          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-          Bucket: env('AWS_BUCKET'),
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
+          params: {
+            ACL: env("AWS_ACL", "public-read"),
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+            Bucket: env("AWS_BUCKET"),
+          },
         },
       },
       actionOptions: {
